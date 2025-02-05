37204c2a60e756a2eb703c51a9f59af014613b8d
Jonas Wiener <wiener.jonas1@gmail.com>
2025-02-05 13:40:20 +0100

    feat: added every currently available spirit feat: added a selector for expansions

--- app/src/main/java/com/example/invader/Randomizer.kt
+++ app/src/main/java/com/example/invader/Randomizer.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -24,6 +25,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconToggleButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -34,6 +36,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ColorMatrix
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -44,6 +48,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import java.util.stream.Collectors
 import kotlin.math.max
 import kotlin.math.min
 
@@ -54,11 +59,13 @@ fun Randomizer() {
   val spirits = remember { mutableStateOf(emptyList<Spirit>()) }
   val num = remember { mutableIntStateOf(2) }
   val easyOnly = remember { mutableStateOf(false) }
+  val expansions = remember { mutableStateOf(listOf(Expansion.Base, Expansion.BranchAndClaw, Expansion.JaggedEarth, Expansion.FeatherAndFlame)) }
 
   val handleRandomizer: () -> Unit = {
-    spirits.value = randomSpirits(num.intValue, easyOnly.value)
+    spirits.value = randomSpirits(num.intValue, easyOnly.value, expansions.value)
     map.value = randomMap(num.intValue)
   }
+
 
   Row(
     horizontalArrangement = Arrangement.SpaceBetween,
@@ -72,6 +79,7 @@ fun Randomizer() {
     Column(
       horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+      ExpansionSelector(expansions.value) { ex -> expansions.value = ex }
       Row(verticalAlignment = Alignment.CenterVertically) {
         Checkbox(checked = easyOnly.value, onCheckedChange = { easyOnly.value = !easyOnly.value })
         Text(stringResource(R.string.easy))
@@ -87,6 +95,35 @@ fun Randomizer() {
       }
       Button(onClick = handleRandomizer) {
         Text(stringResource(R.string.randomize))
+      }
+    }
+  }
+}
+
+
+@Preview
+@Composable
+fun ExpansionSelectorPreview() {
+  ExpansionSelector(listOf(Expansion.Base, Expansion.JaggedEarth, Expansion.BranchAndClaw, Expansion.FeatherAndFlame)) { }
+}
+
+@Composable
+fun ExpansionSelector(expansions: List<Expansion>, onChange: (List<Expansion>) -> Unit) {
+  LazyVerticalGrid(columns = GridCells.Fixed(2),
+    verticalArrangement = Arrangement.spacedBy(10.dp),
+    horizontalArrangement = Arrangement.spacedBy(10.dp),
+    ) {
+    itemsIndexed(Expansion.entries) { _, expansion ->
+      IconToggleButton(
+        checked = expansion in expansions,
+        onCheckedChange = { c -> if (c) onChange(expansions + expansion) else onChange(expansions - expansion) },
+        modifier = Modifier.size(50.dp)
+      ) {
+        Image(painter = painterResource(expansion.icon), stringResource(expansion.desc),
+          colorFilter = ColorFilter.colorMatrix(ColorMatrix().apply { if (expansion !in expansions) setToSaturation(0F) }),
+          modifier = Modifier.clip(RoundedCornerShape(10.dp)).size(50.dp),
+          contentScale = ContentScale.Crop
+        )
       }
     }
   }
@@ -145,9 +182,11 @@ fun TwoPlayerMapStandardPreview() {
 fun TwoPlayerMapStandard(maps: List<Map>) {
   val first = maps[0]
   val second = maps[1]
-  Box(modifier = Modifier
-    .width(300.dp)
-    .height(200.dp), contentAlignment = Alignment.Center) {
+  Box(
+    modifier = Modifier
+      .width(300.dp)
+      .height(200.dp), contentAlignment = Alignment.Center
+  ) {
     Box(
       modifier = Modifier
         .width(196.dp)
@@ -196,9 +235,11 @@ fun TwoPlayerMapFlippedPreview() {
 fun TwoPlayerMapFlipped(maps: List<Map>) {
   val first = maps[0]
   val second = maps[1]
-  Box(modifier = Modifier
-    .width(300.dp)
-    .height(200.dp), contentAlignment = Alignment.Center) {
+  Box(
+    modifier = Modifier
+      .width(300.dp)
+      .height(200.dp), contentAlignment = Alignment.Center
+  ) {
     Box(
       modifier = Modifier
         .width(197.dp)
@@ -248,9 +289,11 @@ fun TwoPlayerMapOpposingPreview() {
 fun TwoPlayerMapOpposing(maps: List<Map>) {
   val first = maps[0]
   val second = maps[1]
-  Box(modifier = Modifier
-    .width(300.dp)
-    .height(200.dp), contentAlignment = Alignment.Center) {
+  Box(
+    modifier = Modifier
+      .width(300.dp)
+      .height(200.dp), contentAlignment = Alignment.Center
+  ) {
     Box(
       modifier = Modifier
         .width(242.dp)
@@ -299,9 +342,11 @@ fun TwoPlayerMapFragmentPreview() {
 fun TwoPlayerMapFragment(maps: List<Map>) {
   val first = maps[0]
   val second = maps[1]
-  Box(modifier = Modifier
-    .height(200.dp)
-    .width(300.dp), contentAlignment = Alignment.Center) {
+  Box(
+    modifier = Modifier
+      .height(200.dp)
+      .width(300.dp), contentAlignment = Alignment.Center
+  ) {
     Box(
       modifier = Modifier
         .width(196.dp)
@@ -347,7 +392,7 @@ fun TwoPlayerMapFragment(maps: List<Map>) {
 @Preview
 @Composable
 fun SpiritsPreview() {
-  Spirits(listOf(Spirit.Fluss, Spirit.Erde))
+  Spirits(listOf(Spirit.Teeth, Spirit.Erde))
 }
 
 @Composable
@@ -367,8 +412,10 @@ fun Spirits(spirits: List<Spirit>) {
         Image(
           painter = painterResource(id = spirit.drawable),
           contentDescription = stringResource(spirit.descResource),
-          modifier = Modifier.clip(RoundedCornerShape(30.dp)),
-          contentScale = ContentScale.Fit
+          modifier = Modifier
+            .clip(RoundedCornerShape(30.dp))
+            .size(150.dp),
+          contentScale = ContentScale.FillBounds,
         )
         Text(
           stringResource(spirit.descResource),
@@ -386,16 +433,60 @@ fun Spirits(spirits: List<Spirit>) {
   }
 }
 
+enum class Expansion(val desc: Int, val icon: Int) {
+  Base(R.string.base, R.drawable.spirit_island),
+  BranchAndClaw(R.string.branchandclaw, R.drawable.branch_and_claw),
+  JaggedEarth(R.string.jaggedearth, R.drawable.jagged_earth),
+  FeatherAndFlame(R.string.featherandflame, R.drawable.feather_and_flame),
+  Horizons(R.string.horizons, R.drawable.horizons),
+  NatureIncarnate(R.string.natureincarnate, R.drawable.nature_incarnate),
+}
 
-enum class Spirit(val drawable: Int, val descResource: Int) {
-  Erde(R.drawable.resilience, R.string.erde),
-  Flamme(R.drawable.madness, R.string.schatten),
-  Blitz(R.drawable.wind, R.string.blitz),
-  Fluss(R.drawable.sunshine, R.string.fluss),
-  Ozean(R.drawable.deeps, R.string.ozean),
-  Angst(R.drawable.violence, R.string.bote),
-  Dahan(R.drawable.tactician, R.string.donner),
-  Wald(R.drawable.tangles, R.string.wald),
+enum class Complexity {
+  Moderate,
+  High,
+  VeryHigh,
+  Low
+}
+
+enum class Spirit(val drawable: Int, val descResource: Int, val expansion: Expansion, val complexity: Complexity) {
+  Erde(R.drawable.resilience, R.string.erde, Expansion.Base, Complexity.Low),
+  Flamme(R.drawable.madness, R.string.schatten, Expansion.Base, Complexity.Low),
+  Blitz(R.drawable.wind, R.string.blitz, Expansion.Base, Complexity.Low),
+  Fluss(R.drawable.sunshine, R.string.fluss, Expansion.Base, Complexity.Low),
+  Ozean(R.drawable.deeps, R.string.ozean, Expansion.Base, Complexity.High),
+  Angst(R.drawable.violence, R.string.bote, Expansion.Base, Complexity.High),
+  Dahan(R.drawable.tactician, R.string.donner, Expansion.Base, Complexity.Moderate),
+  Wald(R.drawable.tangles, R.string.wald, Expansion.Base, Complexity.Moderate),
+  Keeper(R.drawable.keeper, R.string.keeper, Expansion.BranchAndClaw, Complexity.Moderate),
+  Behemoth(R.drawable.behemoth, R.string.behemoth, Expansion.NatureIncarnate, Complexity.Moderate),
+  Darkness(R.drawable.darkness, R.string.darkness, Expansion.NatureIncarnate, Complexity.High),
+  Downpour(R.drawable.downpour, R.string.downpour, Expansion.FeatherAndFlame, Complexity.High),
+  Earthquakes(R.drawable.earthquakes, R.string.earthquakes, Expansion.NatureIncarnate, Complexity.VeryHigh),
+  Eyes(R.drawable.eyes, R.string.eyes, Expansion.Horizons, Complexity.Low),
+  Fangs(R.drawable.fangs, R.string.fangs, Expansion.BranchAndClaw, Complexity.Moderate),
+  Finder(R.drawable.finder, R.string.finder, Expansion.FeatherAndFlame, Complexity.VeryHigh),
+  Fractured(R.drawable.fractured, R.string.fractured, Expansion.JaggedEarth, Complexity.VeryHigh),
+  Gaze(R.drawable.gaze, R.string.gaze, Expansion.NatureIncarnate, Complexity.High),
+  Hearth(R.drawable.hearth, R.string.hearth, Expansion.NatureIncarnate, Complexity.Moderate),
+  Heat(R.drawable.heat, R.string.heat, Expansion.Horizons, Complexity.Low),
+  Lure(R.drawable.lure, R.string.lure, Expansion.JaggedEarth, Complexity.Moderate),
+  Minds(R.drawable.minds, R.string.minds, Expansion.JaggedEarth, Complexity.Moderate),
+  Memory(R.drawable.memory, R.string.memory, Expansion.JaggedEarth, Complexity.Moderate),
+  Mist(R.drawable.mist, R.string.mist, Expansion.JaggedEarth, Complexity.Moderate),
+  Mud(R.drawable.mud, R.string.swamp, Expansion.Horizons, Complexity.Low),
+  Roots(R.drawable.roots, R.string.roots, Expansion.NatureIncarnate, Complexity.Moderate),
+  Serpent(R.drawable.serpent, R.string.serpent, Expansion.FeatherAndFlame, Complexity.High),
+  Starlight(R.drawable.starlight, R.string.starlight, Expansion.JaggedEarth, Complexity.VeryHigh),
+  Stone(R.drawable.stone, R.string.stone, Expansion.JaggedEarth, Complexity.Moderate),
+  Teeth(R.drawable.teeth, R.string.teeth, Expansion.Horizons, Complexity.Low),
+  Trickster(R.drawable.trickster, R.string.trickster, Expansion.JaggedEarth, Complexity.Moderate),
+  Vengeance(R.drawable.vengeance, R.string.vengeance, Expansion.JaggedEarth, Complexity.High),
+  Voice(R.drawable.voice, R.string.voice, Expansion.NatureIncarnate, Complexity.High),
+  Volcano(R.drawable.volcano, R.string.volcano, Expansion.JaggedEarth, Complexity.Moderate),
+  Whirlwind(R.drawable.whirlwind, R.string.whirlwind, Expansion.Horizons, Complexity.Low),
+  Wildfire(R.drawable.wildfire, R.string.wildfire, Expansion.FeatherAndFlame, Complexity.High),
+  Wounded(R.drawable.wounded, R.string.wounded, Expansion.NatureIncarnate, Complexity.High),
 }
 
 enum class Map(val drawable: Int) {
@@ -433,24 +524,20 @@ enum class BoardSetup {
   Fragment,
 }
 
-fun randomSpirits(num: Int, easyOnly: Boolean): List<Spirit> {
-  val allSpirits = mutableListOf(Spirit.Erde, Spirit.Flamme, Spirit.Blitz, Spirit.Fluss, Spirit.Ozean, Spirit.Angst, Spirit.Dahan, Spirit.Wald)
-  val easySpirits = mutableListOf(Spirit.Erde, Spirit.Flamme, Spirit.Blitz, Spirit.Fluss)
-
-  return if (easyOnly) {
-    easySpirits.shuffle()
-    easySpirits.subList(0, num).toList()
-  } else {
-    allSpirits.shuffle()
-    allSpirits.subList(0, num).toList()
+fun randomSpirits(num: Int, onlyLowComplexity: Boolean, expansions: List<Expansion>): List<Spirit> {
+  val spirits = Spirit.entries.stream().filter { s -> s.expansion in expansions && (!onlyLowComplexity || s.complexity == Complexity.Low) }.collect(Collectors.toList())
+  spirits.shuffle()
+  if (spirits.size < num) {
+    return emptyList()
   }
+  return spirits.subList(0, num).toList()
 }
 
 fun randomMap(num: Int): Pair<MutableList<Map>, Orientation> {
   val allMaps = mutableListOf(Map.A, Map.B, Map.C, Map.D, Map.E, Map.F)
   val allOrientations = mutableListOf(Orientation.Down, Orientation.Up)
 
-  var selection = allMaps.subList(0, num)
+  var selection: MutableList<Map>
   do {
     allMaps.shuffle()
     selection = allMaps.subList(0, num)
